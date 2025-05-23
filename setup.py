0132d53264b0d3149fb0f07ac5c354f0a47c73d4
Juan Suarez <82786707+JPSuarezM4@users.noreply.github.com>
2025-05-23 08:34:25 +0000

    actions changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Juan Suarez Dev",
     author_email="juan.suarez@est.iudigital.edu.co",
     description="A package for educational purposes",
-    py_modules=["actividad_1, actividad_2, actividad_3"],
+    py_modules=["actividad_1, actividad_2"],
     install_requires=[
         "pandas",
         "openpyxl",
