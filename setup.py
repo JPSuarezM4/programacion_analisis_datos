2eefb54cfe2a9266ad1e0eaf0d8bfbc9454e09bb
Juan Suarez <82786707+JPSuarezM4@users.noreply.github.com>
2025-05-23 00:04:42 +0000

    empaquetado de dependencias

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup{
+    name="edu_pad",
+    version="0.0.1"
+    author="Juan Suarez Dev"
+    author_email="juan.suarez@est.iudigital.edu.co"
+    description="A package for educational purposes",
+    py_modules=["actividad_1, actividad_2, actividad_3"],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "beautifulsoup4",
+    ],
+}
