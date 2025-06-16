89a91dca93ebabfa6fdf332e9ea79f73e49ad3a1
Juan Suarez <82786707+JPSuarezM4@users.noreply.github.com>
2025-06-16 01:17:48 +0000

    streamlit fixes

--- src/edu_pad/streamlit_pandas.py
+++ src/edu_pad/streamlit_pandas.py
@@ -13,7 +13,7 @@ def main():
     df = pd.read_csv("src/edu_pad/static/csv/data_extractor.csv")
     columnas = ["fecha","abrir","max","min","cerrar","cierre_ajustado","volumen"]
     df_2 = df[columnas]
-    profile = ProfileReport(df_2, title="Dashboard Indicador Dolar")
+    profile = ProfileReport(df_2, title="Dashboard Indicador SOL")
     st.title("Análisis de Datos")
     #st.write(profile.to_html(),unsafe_allow_html=True)
     st.components.v1.html(profile.to_html(), height=600, scrolling=True)
